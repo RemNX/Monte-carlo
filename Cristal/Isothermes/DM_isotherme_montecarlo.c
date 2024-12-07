24db6ba13868e0d302cc27bda290cbb50a400cbb
Kurolycka <150124061+Kurolycka@users.noreply.github.com>
2024-12-07 12:27:41 +0100

    Actualisation

--- Cristal/Isothermes/DM_isotherme_montecarlo.c
+++ Cristal/Isothermes/DM_isotherme_montecarlo.c
@@ -5,15 +5,12 @@
 #include <stdbool.h>
 
 //-----------------------------{Définition des constantes}-----------------------------
-const int nbx_particules = 128;   //Nombre de particules du système
-const int nbx_particules_actives = 128; //Nombre de particules actives du système (sert juste pour définir des défauts)
-const double sigma = 1;                // sigma du lennard jones adimentionné
+const int nbx_particules = 100;   //Nombre de particules du système
 #define Rc 2.5* sigma              //Rayon de coupure 
 #define pi M_PI                     //Constante pi
-#define L  40              // Taille de la boîte
-const double Lmoitie = L * 0.5;    // calcul de la valeur de la moitié de L
-const double rho = nbx_particules_actives / (L * L); // rho la densité volumique
+const int sigma = 1;                // sigma du lennard jones adimentionné
 const double epsilon = 1;          //le E0 du lennard jones adimentionné
+const int dimension = 2; 
 
 /*Calcul pour Rc pour eviter de les refaire, vu qu'ils sont constants*/
 const double Rc2 = Rc * Rc;        
@@ -23,16 +20,13 @@ const double inv_Rc_6 = inv_Rc_2 * inv_Rc_2 * inv_Rc_2;
 const double inv_Rc_10 = inv_Rc_6 * inv_Rc_4;
 const double inv_Rc_12 = inv_Rc_6 * inv_Rc_6;
 
-/*Calcul des terme à ajouter pour les grandeurs calculées, qui sont constants*/
 const double terme_commun_frac = 2/5;
-const double U_tail = pi * epsilon * nbx_particules_actives * rho * (terme_commun_frac * inv_Rc_10 - inv_Rc_4);
 const double U_decal  = 4 * (inv_Rc_12 - inv_Rc_6);
-const double P_tail = 6*pi*epsilon*rho*rho*(terme_commun_frac*inv_Rc_10 - 0.5*inv_Rc_4);
 
 //Je définis le delta max pour le placement aléatoire des particules
-const double delta_max = 0.2*sigma; 
-const double temperature = 0.01; // imposé dans l'exo kT = 0.01
-const int nombre_cycles =100; //1 cycle = 128 tentatives, réussies ou non
+const double delta_max = 0.5*sigma; 
+const double temperature = 1.5; // imposé dans l'exo kT = 0.01
+const int nombre_cycles =3500; //1 cycle = 128 tentatives, réussies ou non
 
 //-----------------------------{Définition de la structure pour la particule}-----------------------------
 /**
@@ -49,6 +43,8 @@ const int nombre_cycles =100; //1 cycle = 128 tentatives, réussies ou non
 typedef struct {
     double x; 
     double y;
+    double fx; 
+    double fy; 
     int actif; //1=active et 0= inactive, permet d'inclure des défauts
 } Particule;
 
@@ -69,29 +65,37 @@ void init_Particule(Particule *p, double x, double y) {
     p->y = y;
 }
 
-/**
- * @brief Initialise une configuration aléatoire de particules dans une boîte carrée de côté L.
- * 
- * Cette fonction remplit un tableau de particules avec des positions et des vitesses générées aléatoirement.
- * Les particules sont positionnées dans une plage de coordonnées allant de -L/2 à L/2.
- * Chaque nouvelle particule respecte une distance minimale spécifiée par rapport aux particules existantes.
- * 
- * @param tab_par Tableau de particules à remplir avec des configurations aléatoires.
- */
-void initialiser_Configuration_aleatoire(Particule tab_par[]) {
+void initialiser_Configuration_Cristalline(Particule tab_par[], double taille_boite){
 
-    // Boucle pour générer nbx_particules particules
-    for (int i = 0; i < nbx_particules; i++) {
-        Particule p; // Nouvelle particule à ajouter au tableau
+    //Calculer le nombre de particules par ligne et par colonne
+    int particules_par_ligne = (int) sqrt(nbx_particules); 
+    int particules_par_colonne = (nbx_particules+particules_par_ligne-1)/particules_par_ligne; 
 
+    //Espacement entre les particules
+    double espacement_x = taille_boite/particules_par_ligne;
+    double espacement_y = taille_boite/particules_par_ligne; 
 
-        // Génère des coordonnées (x, y) aléatoires entre -L/2 et L/2
-        p.x = ((double)rand() / RAND_MAX * L) - Lmoitie;
-        p.y = ((double)rand() / RAND_MAX * L) - Lmoitie;
+    //Calcul de la largeur et hauteur du réseau 
+    double largeur_reseau = espacement_x*(particules_par_ligne-1); 
+    double hauteur_reseau = espacement_y*(particules_par_colonne-1); 
 
-        // Ajoute la particule validée dans le tableau
-        tab_par[i] = p;
-        tab_par[i].actif=1;
+    //Décalage pour centrer le réseau atour de (0,0) 
+    double decalage_x = -largeur_reseau/2.0; 
+    double decalage_y = -hauteur_reseau/2.0; 
+
+    for (int i=0; i<= particules_par_ligne; i++){
+        for (int j=0; j< particules_par_colonne; j++){
+
+            int index = i*particules_par_colonne+j; 
+            if (index<nbx_particules){
+
+                // Position initiale centrée autour de (0, 0)
+                tab_par[index].x = i * espacement_x + decalage_x;
+                tab_par[index].y = j * espacement_y + decalage_y;
+
+                tab_par[index].actif = 1;
+            }
+        }
     }
 }
 
@@ -144,12 +148,12 @@ int change_nouvelle_configuration(Particule tab_par[], int nbx_particules, doubl
  * @param temperature Pointeur vers la variable où sera stockée la température calculée.
  * @param energie_potentielle Pointeur vers la variable où sera stockée l'énergie potentielle calculée.
  */
-double calcul_energie_potentielle_terme_commun(Particule tab_par[], int index_modifie) 
+double calcul_energie_potentielle_terme_commun(Particule tab_par[], int index_modifie, double Lmoitie, double L_boite) 
 {
     /*définition des variables locales à utiliser dans les calculs*/
     double U = 0.0; // energie potentielle
     double rij2, inv_r2, inv_r2x3, inv_r2x6; //puissance de rij
-    double V = L * L; //volume
+    double V = L_boite * L_boite; //volume
 
     for (int i = 0; i < nbx_particules; i++) 
     {
@@ -173,11 +177,11 @@ double calcul_energie_potentielle_terme_commun(Particule tab_par[], int index_mo
                         double abs_disty = fabs(y_sous);
 
                         if (abs_distx > Lmoitie) {
-                            x_sous = - (x_sous / abs_distx) * (L - abs_distx);
+                            x_sous = - (x_sous / abs_distx) * (L_boite - abs_distx);
                         }
 
                         if (abs_disty > Lmoitie) {
-                            y_sous = - (y_sous / abs_disty) * (L - abs_disty);
+                            y_sous = - (y_sous / abs_disty) * (L_boite - abs_disty);
                         }
 
                         /*Calcul de la distance entre deux particules*/
@@ -203,12 +207,12 @@ double calcul_energie_potentielle_terme_commun(Particule tab_par[], int index_mo
 }
 
 
-double calcul_energie_potentielle_terme_modifie(Particule tab_par[], int index_modifie) 
+double calcul_energie_potentielle_terme_modifie(Particule tab_par[], int index_modifie, double Lmoitie, double L_boite) 
 {
     /*définition des variables locales à utiliser dans les calculs*/
     double U_index = 0.0; // energie potentielle
     double rij2, inv_r2, inv_r2x3, inv_r2x6; //puissance de rij
-    double V = L* L; //volume
+    double V = L_boite * L_boite; //volume
     int dimension = 2;
 
     int i = index_modifie; 
@@ -226,11 +230,11 @@ double calcul_energie_potentielle_terme_modifie(Particule tab_par[], int index_m
                 double abs_disty = fabs(y_sous);
 
                 if (abs_distx > Lmoitie) {
-                    x_sous = - (x_sous / abs_distx) * (L - abs_distx);
+                    x_sous = - (x_sous / abs_distx) * (L_boite - abs_distx);
                 }
 
                 if (abs_disty > Lmoitie) {
-                    y_sous = - (y_sous / abs_disty) * (L - abs_disty);
+                    y_sous = - (y_sous / abs_disty) * (L_boite - abs_disty);
                 }
 
                 /*Calcul de la distance entre deux particules*/
@@ -255,14 +259,14 @@ double calcul_energie_potentielle_terme_modifie(Particule tab_par[], int index_m
 
 //-----------------------------{calcul de boltzmann}------------------------------------------
 
-double calcul_boltzmann(Particule tab_ini[], Particule tab_temp[], int index_modifie)
+double calcul_boltzmann(Particule tab_ini[], Particule tab_temp[], int index_modifie, double Lmoitie, double Lboite, double Utail)
 {
     double proba = 0.0;
 
-    double terme_commun = calcul_energie_potentielle_terme_commun(tab_ini, index_modifie) + U_tail;
+    double terme_commun = calcul_energie_potentielle_terme_commun(tab_ini, index_modifie, Lmoitie, Lboite) + Utail;
 
-    double energie_tab_ini = terme_commun + calcul_energie_potentielle_terme_modifie(tab_ini,index_modifie); 
-    double energie_tab_temp = terme_commun + calcul_energie_potentielle_terme_modifie(tab_temp,index_modifie); 
+    double energie_tab_ini = terme_commun + calcul_energie_potentielle_terme_modifie(tab_ini,index_modifie, Lmoitie, Lboite); 
+    double energie_tab_temp = terme_commun + calcul_energie_potentielle_terme_modifie(tab_temp,index_modifie, Lmoitie, Lboite); 
 
     if (energie_tab_temp <= energie_tab_ini){
         proba = 1.0; 
@@ -276,79 +280,183 @@ double calcul_boltzmann(Particule tab_ini[], Particule tab_temp[], int index_mod
 
 
 //-----------------------------{Enregistrer les positions}-------------------------------------------
-void enregistrer_positions(FILE *fp, Particule *tab_par, int tentative, int nbx_particules){
-    fprintf(fp, "%d\n", nbx_particules);
-    fprintf(fp, "\n");
-    for (int i=0; i<nbx_particules; i++){
-        fprintf(fp, "A %f %f\n", tab_par[i].x, tab_par[i].y);
-    }
+
+void enregistrer_pression(FILE *fp, double pression, double volume){
+    fprintf(fp, "%f : %f\n", volume ,pression);
 }
+
+
+//-----------------------------{Calcul_forces}--------------------------------------------------------
+/**
+ * @brief Calcule les forces entre toutes les paires de particules dans le système.
+ *
+ * Cette fonction parcourt le tableau des particules et calcule les forces d'interaction
+ * entre toutes les paires, mettant ainsi à jour les composantes de force de chaque particule.
+ * Le calcul utilise des conditions périodiques aux bords pour gérer les interactions
+ * et contribue également au calcul du viriel, utilisé dans le calcul de la pression.
+ *
+ * @param tab_par Tableau contenant les particules du système, chacune avec ses
+ *                attributs de position, de vitesse, et de force.
+ * @param viriel Pointeur vers une variable où le viriel total du système sera stocké.
+ *               Le viriel est une somme pondérée des forces entre particules et sert
+ *               dans les calculs thermodynamiques (notamment la pression).
+ *
+ * @note La fonction suppose que `tab_par` est initialisé avec les particules en positions
+ *       valides dans l'espace de simulation.
+ */
+double calcul_viriel(Particule tab_par[], double Lmoitie, double Lboite) {
+    // Initialiser le viriel (sert pour le calcul de la pression)
+    double viriel = 0.0;
+
+    // Réinitialiser les forces et l'énergie potentielle
+    for (int i = 0; i < nbx_particules; i++) {
+        tab_par[i].fx = 0.0;
+        tab_par[i].fy = 0.0;
+    }
+
+    // Calcul des forces d'interaction entre les particules
+    for (int i = 0; i < nbx_particules; i++) {
+        if (!tab_par[i].actif) continue;
+
+        for (int j = i + 1; j < nbx_particules; j++) {
+            if (!tab_par[j].actif) continue;
+
+            double x_sous = tab_par[i].x - tab_par[j].x;
+            double y_sous = tab_par[i].y - tab_par[j].y;
+
+            double abs_distx = fabs(x_sous);
+            double abs_disty = fabs(y_sous);
+
+            if (abs_distx > Lmoitie) {
+                x_sous = - (x_sous / abs_distx) * (Lboite - abs_distx);
+            }
+
+            if (abs_disty > Lmoitie) {
+                y_sous = - (y_sous / abs_disty) * (Lboite - abs_disty);
+            }
+
+            double rij2 = x_sous * x_sous + y_sous * y_sous;
+
+            if (rij2 < Rc2) {
+                double r6 = rij2 * rij2 * rij2;
+                double r12 = r6 * r6;
+                double inv_r2 = 1.0 / rij2;
+                double inv_r6 = 1.0 / r6;
+                double inv_r12 = 1.0 / r12;
+                double terme_commun = 24.0 * (2 * inv_r12 - inv_r6) * inv_r2;
+
+                double force_x = terme_commun * x_sous;
+                double force_y = terme_commun * y_sous;
+
+                tab_par[i].fx += force_x;
+                tab_par[j].fx -= force_x;
+                tab_par[i].fy += force_y;
+                tab_par[j].fy -= force_y;
+
+                viriel += terme_commun * rij2;
+            }
+        }
+    }
+
+    return viriel; 
+}
+
 
 
 //-----------------------------{main}----------------------------------------------------------------
 int main() 
 {
-
     // Initialiser le générateur de nombres aléatoires avec une graine pour avoir des résultats reproductibles
     srand(time(NULL));
 
-    // Creation d'un tableau de particules
-    Particule tab_particules[nbx_particules];
-    Particule tab_particules_temporaire[nbx_particules]; 
+    double energie_totale[nombre_cycles+1]; // Je cherche à voir au bout de combien de cycles j'atteinds l'équilibre
 
+    double energie_bloc=0.0; 
+    double pression;
+    double viriel; 
 
-    // Initialisation des particules avec une configuration aleatoire
-    initialiser_Configuration_aleatoire(tab_particules);
+    int L_variations[20]; 
+    double rho_variation[20]; 
+    double L_moitie[20];
+    double volume_variations[20];
+    double U_tail_variations[20];
+    double P_tail_variations[20];
 
-    printf("aaa \n");
+    for (int i=0; i<20; i++){
+        double s=10+i*1; 
+        L_variations[i]=10+s; 
+        L_moitie[i] = L_variations[i]*0.5; 
+        volume_variations[i] = L_variations[i]*L_variations[i]; 
+        rho_variation[i] = nbx_particules/volume_variations[i];
+        U_tail_variations[i] = pi * epsilon * nbx_particules * rho_variation[i] * (terme_commun_frac * inv_Rc_10 - inv_Rc_4);
+        P_tail_variations[i] = 6*pi*epsilon*rho_variation[i]*rho_variation[i]*(terme_commun_frac*inv_Rc_10 - 0.5*inv_Rc_4);
+    }
     
     // Ouvre le fichier "positions_data.txt" en mode écriture ("w")
     // Ce fichier stockera les positions des particules au fil du temps dans le format suivant:
     // temps   x1 y1    x2 y2    x3 y3    x4 y4.....
-    FILE *pos_file = fopen("positions_data.txt", "w");
+    //FILE *pos_file = fopen("positions_data.txt", "w");
+    //FILE *energie_file = fopen("energie_data_alter.txt", "w"); 
+    FILE *pression_file_volume = fopen("pression_data_1-5.txt","w"); 
 
-    int tentative = 0;
-    int tentative_acceptee=0; 
-    int index_modidi=0; 
+    for (int i=0; i<20; i++){
 
-    // Appelle la fonction enregistrer_Positions pour écrire les positions initiales des particules
-    enregistrer_positions(pos_file, tab_particules, tentative, nbx_particules);
+        double stock_bloc_pression=0; 
+        int taille_bloc=10; 
 
-    for (int cycle=0 ; cycle<nombre_cycles; cycle++){
+        // Creation d'un tableau de particules
+        Particule tab_particules[nbx_particules];
+        Particule tab_particules_temporaire[nbx_particules]; 
 
-        printf("Cycle : %d\n", cycle); 
+        printf("Boucle i : %d\n",i);
+
+        // Initialisation des particules avec une configuration aleatoire
+        initialiser_Configuration_Cristalline(tab_particules, L_variations[i]); 
+
+        int tentative = 0;
+        int tentative_acceptee=0; 
+        int index_modidi=0; 
+
+        for (int cycle=0 ; cycle<nombre_cycles; cycle++){
 
             for (int tentativou=0; tentativou < nbx_particules ; tentativou++){
 
-                for (int i=0; i<nbx_particules; i++){
-                    tab_particules_temporaire[i]=tab_particules[i];
+                for (int s=0; s<nbx_particules; s++){
+                    tab_particules_temporaire[s]=tab_particules[s];
                 }
 
-                int index_modidi = change_nouvelle_configuration(tab_particules_temporaire, nbx_particules, Lmoitie, L); 
-                double proba_finale=calcul_boltzmann(tab_particules, tab_particules_temporaire,index_modidi); 
+                int index_modidi = change_nouvelle_configuration(tab_particules_temporaire, nbx_particules, L_moitie[i], L_variations[i]); 
+                double proba_finale=calcul_boltzmann(tab_particules, tab_particules_temporaire,index_modidi, L_moitie[i], L_variations[i], U_tail_variations[i]);
 
                 double x= ((double)rand() / RAND_MAX);
 
                 if (proba_finale >= x){
 
-                    tentative_acceptee += 1;
+                    tentative_acceptee += 1; 
 
                     for (int i=0; i<nbx_particules; i++){
                         tab_particules[i]=tab_particules_temporaire[i];
                     }
                 }
 
+                viriel=calcul_viriel(tab_particules, L_moitie[i], L_variations[i]);
+                pression = (nbx_particules * (temperature) / volume_variations[i]) + (viriel / (dimension*volume_variations[i]*nbx_particules))+P_tail_variations[i];
+
                 tentative += 1; 
 
             }
 
-        enregistrer_positions(pos_file, tab_particules, tentative, nbx_particules); 
+            if (cycle>=(nombre_cycles-1-taille_bloc)){
+                stock_bloc_pression += pression;
+            }
+
+        }
+
+        stock_bloc_pression=stock_bloc_pression/taille_bloc; 
+        enregistrer_pression(pression_file_volume, stock_bloc_pression, volume_variations[i]);
+
     }
-
-    fclose(pos_file);  // De même pour le fichier positions_data.txt
-
-    double taux_acceptation = (double)tentative_acceptee/tentative; 
-    printf("Taux d'acceptation : %f", taux_acceptation); 
+    fclose(pression_file_volume);
 
     return 0;
 }
